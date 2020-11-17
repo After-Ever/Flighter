80ebe3f5a7cf30738aa14d63017ecac2ea5d0617
Ben Trotter <bendavidtrotter@gmail.com>
2020-11-17 15:51:55 -0500

    Provides support for world based display rects.

--- FlighterUnity/InputPoller.cs
+++ FlighterUnity/InputPoller.cs
@@ -1,18 +1,27 @@
-﻿using Flighter;
+﻿using System;
+
+using Flighter;
 using Flighter.Input;
 using KeyCode = Flighter.Input.KeyCode;
 using Input = UnityEngine.Input;
+using UnityEngine;
 
 namespace FlighterUnity
 {
     public class InputPoller : IInputPoller, IKeyInputPoller, IMouseInputPoller
     {
+        readonly Camera camera;
+        readonly RectTransform rootRect;
+
         Point lastPosition = Point.Zero;
 
-        // TODO: Accept a world ui thing and camera pair to make this a world poller. 
-        public InputPoller()
+        public InputPoller(Camera camera = null, RectTransform rootRect = null)
         {
+            if ((camera == null) != (rootRect == null))
+                throw new ArgumentNullException("Must provider either both camera and rootRect, or neither.");
 
+            this.camera = camera;
+            this.rootRect = rootRect;
         }
 
         /// <summary>
@@ -25,6 +34,7 @@ namespace FlighterUnity
         }
 
         // IInputPoller
+
         public IKeyInputPoller KeyPoller => this;
         public IMouseInputPoller MousePoller => this;
 
@@ -32,8 +42,7 @@ namespace FlighterUnity
 
         public float ScrollDelta => Input.mouseScrollDelta.y;
         public Point PositionDelta => Position - lastPosition;
-        // TODO: Get more complex if based on a world tree.
-        public Point Position => Input.mousePosition.ToPoint();
+        public Point Position => ScreenPosToDisplayRectPos(Input.mousePosition);
 
         public bool GetButton(MouseButton button)
         {
@@ -65,6 +74,41 @@ namespace FlighterUnity
         public bool GetKeyUp(KeyCode key)
         {
             return Input.GetKeyUp(key.ToUnity());
+        }
+
+        /// <summary>
+        /// Returns (-1, -1) when bad! TODO, make better description.
+        /// </summary>
+        /// <param name="screenPos"></param>
+        /// <returns></returns>
+        Point ScreenPosToDisplayRectPos(Vector3 screenPos)
+        {
+            if (camera == null)
+            {
+                var point = screenPos.ToPoint();
+                point.y = Screen.height - point.y;
+                return point;
+            }
+
+            var pointerRay = camera.ScreenPointToRay(screenPos);
+            var rectOrigin = rootRect.position;
+            var rectRight = rootRect.right;
+            var rectDown = -rootRect.up;
+            var rectNormal = rootRect.forward;
+
+            var inDirection = Vector3.Dot(rectNormal, pointerRay.direction);
+
+            if (inDirection <= 0)
+                return new Point(-1, -1);
+
+            var d = Vector3.Dot(rectNormal, pointerRay.origin - rectOrigin) / inDirection;
+
+            var pointOnRect = (pointerRay.origin + pointerRay.direction * d) - rectOrigin;
+
+            var x = Vector3.Dot(pointOnRect, rectRight);
+            var y = Vector3.Dot(pointOnRect, rectDown);
+
+            return new Point(x, y);
         }
     }
 }
--- FlighterUnity/InputProvider.cs
+++ FlighterUnity/InputProvider.cs
@@ -13,10 +13,15 @@ namespace FlighterUnity
         readonly InputPoller poller;
         readonly Input input;
 
-        // TODO Accept world widget tree and camera!
-        public InputProvider()
+        /// <summary>
+        /// </summary>
+        /// <param name="camera">Include this and <paramref name="rootRect"/> to make positions relative to a world rect.
+        /// Otherwise, screen space will be used.</param>
+        /// <param name="rootRect">Include this and <paramref name="camera"/> to make positions relative to a world rect.
+        /// Otherwise, screen space will be used.</param>
+        public InputProvider(Camera camera, RectTransform rootRect)
         {
-            poller = new InputPoller();
+            poller = new InputPoller(camera, rootRect);
             input = new Input(poller);
         }
 
