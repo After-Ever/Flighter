57216e9f22f9d871c1cd50b411a76524be87f301
Ben Trotter <bendavidtrotter@gmail.com>
2020-11-20 16:15:25 -0500

    Declared utility methods. No implementation yet.

---
+++ FlighterUnity/Display.cs
@@ -0,0 +1,50 @@
+﻿using Flighter;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace FlighterUnity
+{
+    public class DisplayHandle
+    {
+        /// <summary>
+        /// Tearn down this display.
+        /// </summary>
+        public void TearDown()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public static class Display
+    {
+        /// <summary>
+        /// Instrument <paramref name="widget"/> on the screen.
+        /// Multiple calls to this will stack the widgets on one another, in call order
+        /// (recent ontop of older).
+        /// </summary>
+        /// <param name="widget"></param>
+        /// <param name="pixelScale">How many logical pixels per screen pixel.</param>
+        /// <returns></returns>
+        public static DisplayHandle OnScreen(Widget widget, float pixelScale = 1)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static DisplayHandle InWorld(Widget widget, Vector3 position, Quaternion rotation, float pixelsPerUnit, bool facingCam = false)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static DisplayHandle OnTransform(Widget widget, Transform transform, float pixelsPerUnit, bool facingCam = false)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static DisplayHandle ToRenderTexture(Widget widget, Size size, RenderTexture target)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
