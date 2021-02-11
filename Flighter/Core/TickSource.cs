2512c69b4a88515afb199aad6a59d0ecd03a0426
Ben Trotter <ben@aftev.com>
2021-02-11 15:28:36 -0500

    Forgot to commit TickerSource...

---
+++ Flighter/Core/TickSource.cs
@@ -0,0 +1,27 @@
+﻿namespace Flighter.Core
+{
+    public class TickSource : InheritedWidget
+    {
+        public readonly TickProvider tickProvider;
+
+        public TickSource(Widget child, TickProvider tickProvider)
+            : base(child) 
+        {
+            this.tickProvider = tickProvider;
+        }
+
+        public static TickSource Of(BuildContext context)
+            => context.GetInheritedWidgetOfExactType<TickSource>();
+
+        public static TickSource operator +(TickSource tickSource, OnTick onTick)
+        {
+            tickSource.tickProvider.Tick += onTick;
+            return tickSource;
+        }
+        public static TickSource operator -(TickSource tickSource, OnTick onTick)
+        {
+            tickSource.tickProvider.Tick -= onTick;
+            return tickSource;
+        }
+    }
+}
